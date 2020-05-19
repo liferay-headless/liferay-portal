a88014e7dbe162e23aaeec98cb679feb1cfb32dc
Javier de Arcos <javier.dearcos@liferay.com>
2020-05-19 16:00:21 +0200

    LPS-113914 Fix generation error in REST Builder when using schema identifier name as {schemaName}+Id

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/FreeMarkerTool.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/FreeMarkerTool.java
@@ -373,7 +373,7 @@ public class FreeMarkerTool {
 					for (String propertyName : propertySchemas.keySet()) {
 						if (_isGraphQLPropertyRelation(
 								javaMethodSignature, parameterName,
-								propertyName)) {
+								propertyName, entry.getKey())) {
 
 							javaMethodSignatureMap.put(
 								methodName,
@@ -995,13 +995,13 @@ public class FreeMarkerTool {
 
 	private boolean _isGraphQLPropertyRelation(
 		JavaMethodSignature javaMethodSignature, String parameterName,
-		String propertyName) {
+		String propertyName, String schemaName) {
 
 		String returnType = StringUtil.toLowerCase(
 			javaMethodSignature.getReturnType());
-		String schemaName = StringUtil.removeSubstring(parameterName, "Id");
 
-		if (propertyName.equals(parameterName) &&
+		if (!StringUtil.equalsIgnoreCase(propertyName, schemaName + "Id") &&
+			StringUtil.equals(propertyName, parameterName) &&
 			returnType.endsWith(StringUtil.toLowerCase(schemaName))) {
 
 			return true;
