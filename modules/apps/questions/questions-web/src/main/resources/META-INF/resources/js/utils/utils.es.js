a498a44795489c0f63d9784d53cdd53249f00a51
Luismi Barcos <luismiguel.barcos@liferay.com>
2020-09-23 11:41:36 +0200

    LPS-121262 Handle hyphens in topics names

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/utils/utils.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/utils/utils.es.js
@@ -123,14 +123,21 @@ export function normalize(ratingValue) {
 
 export function stringToSlug(text) {
 	const whiteSpaces = /\s+/g;
+	const hyphens = /-+/g;
+	text = text.toLowerCase();
+	text = text.replace(hyphens, '--');
+	text = text.replace(whiteSpaces, '-');
 
-	return text.replace(whiteSpaces, '-').toLowerCase();
+	return text;
 }
 
 export function slugToText(slug) {
-	const hyphens = /-+/g;
+	const hyphens = /(?<!-)-(?!-.)/g;
+	const encodedHyphens = /--+/g;
+	slug = slug.replace(hyphens, ' ');
+	slug = slug.replace(encodedHyphens, '-').toLowerCase();
 
-	return slug.replace(hyphens, ' ').toLowerCase();
+	return slug;
 }
 
 export function historyPushWithSlug(push) {
