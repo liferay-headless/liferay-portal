9b7f000794877b65f1e313964b9038cd5181e15f
Luismi Barcos <luismiguel.barcos@liferay.com>
2020-11-20 13:19:42 +0100

    LPS-114274 Add implementation to retrieve the list of structured contents with their last versions

--- modules/apps/headless/headless-admin-content/headless-admin-content-impl/src/main/java/com/liferay/headless/admin/content/internal/resource/v1_0/StructuredContentResourceImpl.java
+++ modules/apps/headless/headless-admin-content/headless-admin-content-impl/src/main/java/com/liferay/headless/admin/content/internal/resource/v1_0/StructuredContentResourceImpl.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
+ * <p>
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -14,9 +14,33 @@
 
 package com.liferay.headless.admin.content.internal.resource.v1_0;
 
+import com.liferay.headless.admin.content.dto.v1_0.StructuredContentVersionInformation;
+import com.liferay.headless.admin.content.internal.dto.v1_0.util.StructuredContentVersionInformationBuilder;
+import com.liferay.headless.admin.content.internal.dto.v1_0.util.VersionInformationUtil;
 import com.liferay.headless.admin.content.resource.v1_0.StructuredContentResource;
+import com.liferay.headless.delivery.dto.v1_0.StructuredContent;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleService;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.vulcan.aggregation.Aggregation;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.util.ContentLanguageUtil;
+import com.liferay.portal.vulcan.util.SearchUtil;
+
+import java.util.Collections;
+
+import javax.validation.constraints.NotNull;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -28,4 +52,89 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class StructuredContentResourceImpl
 	extends BaseStructuredContentResourceImpl {
+
+	@Override
+	public Page<StructuredContent>
+			getSiteStructuredContentsVersionInformationPage(
+				@NotNull Long siteId, Boolean flatten, String search,
+				Aggregation aggregation, Filter filter, Pagination pagination,
+				Sort[] sorts)
+		throws Exception {
+
+		return SearchUtil.search(
+			null,
+			booleanQuery -> {
+			},
+			filter, JournalArticle.class, search, pagination,
+			queryConfig -> queryConfig.setSelectedFieldNames(
+				Field.ARTICLE_ID, Field.SCOPE_GROUP_ID),
+			searchContext -> {
+				searchContext.addVulcanAggregation(aggregation);
+				searchContext.setAttribute(
+					Field.STATUS, WorkflowConstants.STATUS_APPROVED);
+				searchContext.setAttribute("head", Boolean.TRUE);
+				searchContext.setCompanyId(contextCompany.getCompanyId());
+
+				if (siteId != null) {
+					searchContext.setGroupIds(new long[] {siteId});
+				}
+			},
+			sorts,
+			document -> {
+				JournalArticle journalArticle = _journalArticleService.getLatestArticle(
+					GetterUtil.getLong(document.get(Field.SCOPE_GROUP_ID)),
+					document.get(Field.ARTICLE_ID),
+					WorkflowConstants.STATUS_ANY);
+				return _toStructuredContentVersionInformation(journalArticle);
+			});
+	}
+
+	private StructuredContent _toStructuredContent(
+			JournalArticle journalArticle)
+		throws Exception {
+
+		ContentLanguageUtil.addContentLanguageHeader(
+			journalArticle.getAvailableLanguageIds(),
+			journalArticle.getDefaultLanguageId(), contextHttpServletResponse,
+			contextAcceptLanguage.getPreferredLocale());
+
+		DTOConverter<JournalArticle, StructuredContent> dtoConverter =
+			(DTOConverter<JournalArticle, StructuredContent>)
+				_dtoConverterRegistry.getDTOConverter(
+					JournalArticle.class.getName());
+
+		if (dtoConverter == null) {
+			return null;
+		}
+
+		return dtoConverter.toDTO(
+			new DefaultDTOConverterContext(
+				contextAcceptLanguage.isAcceptAllLanguages(),
+				Collections.emptyMap(), _dtoConverterRegistry,
+				contextHttpServletRequest, journalArticle.getResourcePrimKey(),
+				contextAcceptLanguage.getPreferredLocale(), contextUriInfo,
+				contextUser),
+			journalArticle);
+	}
+
+	private StructuredContentVersionInformation
+	_toStructuredContentVersionInformation(
+		JournalArticle journalArticle)
+		throws Exception {
+
+		return new StructuredContentVersionInformationBuilder(
+			_toStructuredContent(journalArticle)
+		).setVersionInformation(
+			VersionInformationUtil.toVersionInformation(
+				journalArticle.getGroupId(), journalArticle.getStatus(),
+				journalArticle.getVersion())
+		).build();
+	}
+
+	@Reference
+	private DTOConverterRegistry _dtoConverterRegistry;
+
+	@Reference
+	private JournalArticleService _journalArticleService;
+
 }
