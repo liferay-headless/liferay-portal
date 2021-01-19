bdaaa45d26e63c478596722b13b790a888b86904
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-01-19 16:11:29 +0100

    LPS-125599 Implement endpoint for retrieving a specific display page template

--- modules/apps/headless/headless-admin-content/headless-admin-content-impl/src/main/java/com/liferay/headless/admin/content/internal/resource/v1_0/DisplayPageTemplateResourceImpl.java
+++ modules/apps/headless/headless-admin-content/headless-admin-content-impl/src/main/java/com/liferay/headless/admin/content/internal/resource/v1_0/DisplayPageTemplateResourceImpl.java
@@ -23,6 +23,7 @@ import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalServ
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryService;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
@@ -47,6 +48,27 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class DisplayPageTemplateResourceImpl
 	extends BaseDisplayPageTemplateResourceImpl {
+
+	@Override
+	public DisplayPageTemplate getSiteDisplayPageTemplate(
+			Long siteId, String displayPageTemplateKey)
+		throws Exception {
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			_layoutPageTemplateEntryService.fetchLayoutPageTemplateEntry(
+				siteId, displayPageTemplateKey);
+
+		if ((layoutPageTemplateEntry == null) ||
+			(layoutPageTemplateEntry.getType() !=
+				LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE)) {
+
+			throw new PortalException(
+				"Invalid Display Page Template with key: " +
+					displayPageTemplateKey);
+		}
+
+		return _toDisplayPageTemplate(layoutPageTemplateEntry);
+	}
 
 	@Override
 	public Page<DisplayPageTemplate> getSiteDisplayPageTemplatesPage(
