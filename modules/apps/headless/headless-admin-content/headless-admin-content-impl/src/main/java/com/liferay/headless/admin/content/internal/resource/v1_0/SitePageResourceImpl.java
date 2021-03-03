040897ba5a0e881172469f93e5d6a40f896fbfc0
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-03-03 13:26:53 +0100

    LPS-128595 Implement endpoint for deleting a site page

--- modules/apps/headless/headless-admin-content/headless-admin-content-impl/src/main/java/com/liferay/headless/admin/content/internal/resource/v1_0/SitePageResourceImpl.java
+++ modules/apps/headless/headless-admin-content/headless-admin-content-impl/src/main/java/com/liferay/headless/admin/content/internal/resource/v1_0/SitePageResourceImpl.java
@@ -14,9 +14,20 @@
 
 package com.liferay.headless.admin.content.internal.resource.v1_0;
 
+import com.liferay.friendly.url.model.FriendlyURLEntryLocalization;
+import com.liferay.friendly.url.service.FriendlyURLEntryLocalService;
 import com.liferay.headless.admin.content.resource.v1_0.SitePageResource;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+import com.liferay.portal.kernel.service.LayoutFriendlyURLLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.LayoutService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.Portal;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +38,44 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = SitePageResource.class
 )
 public class SitePageResourceImpl extends BaseSitePageResourceImpl {
+
+	@Override
+	public void deleteSiteSitePage(Long siteId, String friendlyUrlPath)
+		throws Exception {
+
+		Layout layout = _getLayout(siteId, friendlyUrlPath);
+
+		_layoutService.deleteLayout(layout.getPlid(), new ServiceContext());
+	}
+
+	private Layout _getLayout(long groupId, String friendlyUrlPath)
+		throws Exception {
+
+		String resourceName = ResourceActionsUtil.getCompositeModelName(
+			Layout.class.getName(), "false");
+
+		FriendlyURLEntryLocalization friendlyURLEntryLocalization =
+			_friendlyURLEntryLocalService.getFriendlyURLEntryLocalization(
+				groupId, _portal.getClassNameId(resourceName),
+				StringPool.FORWARD_SLASH + friendlyUrlPath);
+
+		return _layoutLocalService.getLayout(
+			friendlyURLEntryLocalization.getClassPK());
+	}
+
+	@Reference
+	private FriendlyURLEntryLocalService _friendlyURLEntryLocalService;
+
+	@Reference
+	private LayoutFriendlyURLLocalService _layoutFriendlyURLLocalService;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private LayoutService _layoutService;
+
+	@Reference
+	private Portal _portal;
+
 }
