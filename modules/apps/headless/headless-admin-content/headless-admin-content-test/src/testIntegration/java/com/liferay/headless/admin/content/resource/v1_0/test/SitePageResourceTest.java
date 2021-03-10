f7d7c3aba94b0b96b6130ef92cd18d2a7e8c5351
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-03-10 14:30:24 +0100

    LPS-128595 Implement tests

--- modules/apps/headless/headless-admin-content/headless-admin-content-test/src/testIntegration/java/com/liferay/headless/admin/content/resource/v1_0/test/SitePageResourceTest.java
+++ modules/apps/headless/headless-admin-content/headless-admin-content-test/src/testIntegration/java/com/liferay/headless/admin/content/resource/v1_0/test/SitePageResourceTest.java
@@ -15,14 +15,80 @@
 package com.liferay.headless.admin.content.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.test.rule.Inject;
 
-import org.junit.Ignore;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Javier Gamarra
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class SitePageResourceTest extends BaseSitePageResourceTestCase {
+
+	@Override
+	@Test
+	public void testDeleteSiteSitePage() throws Exception {
+		Layout layout = _addLayout(testGroup.getGroupId());
+
+		String friendlyURL = layout.getFriendlyURL();
+
+		sitePageResource.deleteSiteSitePage(
+			testGroup.getGroupId(), friendlyURL.substring(1));
+
+		Assert.assertNull(_layoutLocalService.fetchLayout(layout.getPlid()));
+	}
+
+	@Override
+	@Test
+	public void testGraphQLDeleteSiteSitePage() throws Exception {
+		Layout layout = _addLayout(testGroup.getGroupId());
+
+		String friendlyURL = layout.getFriendlyURL();
+
+		JSONObject jsonObject = invokeGraphQLMutation(
+			new GraphQLField(
+				"admin",
+				new GraphQLField(
+					"deleteSiteSitePage",
+					HashMapBuilder.<String, Object>put(
+						"friendlyUrlPath",
+						"\"" + friendlyURL.substring(1) + "\""
+					).put(
+						"siteKey", "\"" + testGroup.getGroupId() + "\""
+					).build())));
+
+		JSONObject dataJSONObject = jsonObject.getJSONObject("data");
+
+		Assert.assertNotNull(dataJSONObject);
+
+		Assert.assertTrue(
+			JSONUtil.getValueAsBoolean(
+				dataJSONObject, "JSONObject/admin",
+				"Object/deleteSiteSitePage"));
+	}
+
+	private Layout _addLayout(long groupId) throws Exception {
+		return _layoutLocalService.addLayout(
+			TestPropsValues.getUserId(), groupId, false,
+			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			StringPool.BLANK, LayoutConstants.TYPE_CONTENT, false, null,
+			ServiceContextTestUtil.getServiceContext(groupId));
+	}
+
+	@Inject
+	private LayoutLocalService _layoutLocalService;
+
 }
