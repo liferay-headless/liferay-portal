1afcd5d7f3991aabe42024438fe7998ab96c5f46
Jair Medeiros <jair.medeiros@liferay.com>
2023-06-06 14:42:21 -0300

    LPS-185759 SF

--- modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/util/CreatorUtil.java
+++ modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/util/CreatorUtil.java
@@ -43,6 +43,7 @@ public class CreatorUtil {
 				contentType = "UserAccount";
 				familyName = user.getLastName();
 				givenName = user.getFirstName();
+
 				id = user.getUserId();
 				name = user.getFullName();
 
