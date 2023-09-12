8f0c3911b62605b970727ee5dd1cec0def058fde
sergiojimcos <sergio.jdelcoso@liferay.com>
2023-09-12 15:45:53 +0200

    LPS-194572 Solve problem of initializing Company and add new validation checking that actions exported are not empty

--- modules/apps/batch-planner/batch-planner-test/src/testIntegration/java/com/liferay/batch/planner/batch/engine/broker/test/BatchEngineBrokerTest.java
+++ modules/apps/batch-planner/batch-planner-test/src/testIntegration/java/com/liferay/batch/planner/batch/engine/broker/test/BatchEngineBrokerTest.java
@@ -76,6 +76,7 @@ import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.jackson.databind.ser.VulcanPropertyFilter;
@@ -139,6 +140,8 @@ public class BatchEngineBrokerTest {
 			TestPropsValues.getUserId());
 
 		_company2 = CompanyTestUtil.addCompany();
+
+		PortalInstances.initCompany(_company2);
 
 		User user = UserTestUtil.getAdminUser(_company2.getCompanyId());
 
@@ -223,14 +226,29 @@ public class BatchEngineBrokerTest {
 			ServiceContextTestUtil.getServiceContext(companyId, 0, userId));
 	}
 
+	private void _assertActionsContentNotNull(JsonNode actionJsonNode) {
+		String[] actionKeys = {"delete", "get", "permissions", "update"};
+
+		for (String actionKey : actionKeys) {
+			JsonNode jsonNode = actionJsonNode.get(actionKey);
+
+			Assert.assertTrue(!jsonNode.isEmpty());
+		}
+	}
+
 	private void _assertEquals(JsonNode expectedJsonNode, JsonNode jsonNode) {
 		for (String fieldName : _fieldNames) {
 			JsonNode expectedFieldJsonNode = expectedJsonNode.get(fieldName);
 
 			JsonNode fieldJsonNode = jsonNode.get(fieldName);
 
-			Assert.assertEquals(
-				expectedFieldJsonNode.toString(), fieldJsonNode.toString());
+			if (Objects.equals(fieldName, "actions")) {
+				_assertActionsContentNotNull(fieldJsonNode);
+			}
+			else {
+				Assert.assertEquals(
+					expectedFieldJsonNode.toString(), fieldJsonNode.toString());
+			}
 		}
 	}
 
@@ -489,7 +507,7 @@ public class BatchEngineBrokerTest {
 	}
 
 	private static final List<String> _fieldNames = Arrays.asList(
-		"dateCreated", "dateModified", "externalReferenceCode", "id",
+		"actions", "dateCreated", "dateModified", "externalReferenceCode", "id",
 		"testAttachmentField", "testBooleanField", "testDateField",
 		"testDateTimeField", "testDecimalField", "testIntegerField",
 		"testLongIntegerField", "testLongTextField",
