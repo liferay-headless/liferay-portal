35ce473761f007bd7bcd3c29bb2c8d088c7d797b
Carlos Correa <carlos.correa@liferay.com>
2023-11-17 09:14:55 +0100

    LPS-201502 auto SF

--- modules/apps/object/object-api/src/main/java/com/liferay/object/field/attachment/AttachmentManager.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/field/attachment/AttachmentManager.java
@@ -19,13 +19,13 @@ import com.liferay.portal.kernel.service.ServiceContext;
 public interface AttachmentManager {
 
 	public FileEntry addFileEntry(
-		long companyId, byte[] fileContent, String fileName,
-		String folderExternalReferenceCode, long groupId,
-		long objectFieldId, ServiceContext serviceContext)
+			long companyId, byte[] fileContent, String fileName, long groupId,
+			long objectFieldId, ServiceContext serviceContext)
 		throws Exception;
 
 	public FileEntry addFileEntry(
-			long companyId, byte[] fileContent, String fileName, long groupId,
+			long companyId, byte[] fileContent, String fileName,
+			String folderExternalReferenceCode, long groupId,
 			long objectFieldId, ServiceContext serviceContext)
 		throws Exception;
 
