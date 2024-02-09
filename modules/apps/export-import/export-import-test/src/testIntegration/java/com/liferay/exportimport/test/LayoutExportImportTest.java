601ea930b233afc5b6de97e25c8c1c3de5406e41
Daniel Szimko <daniel.szimko@liferay.com>
2024-02-09 14:49:24 +0100

    LPD-17732 add FeatureFlags annotation on relevant test classes

--- modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutExportImportTest.java
+++ modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutExportImportTest.java
@@ -39,6 +39,7 @@ import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.staging.configuration.StagingConfiguration;
@@ -62,6 +63,7 @@ import org.junit.runner.RunWith;
 /**
  * @author Eduardo García
  */
+@FeatureFlags("LPS-199086")
 @RunWith(Arquillian.class)
 public class LayoutExportImportTest extends BaseExportImportTestCase {
 
