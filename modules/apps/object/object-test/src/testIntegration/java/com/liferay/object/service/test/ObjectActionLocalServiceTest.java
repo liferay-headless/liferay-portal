13f9269afa574affd9cce3a5806e2d63c34ff328
Carlos Correa <carlos.correa@liferay.com>
2024-09-27 14:58:55 +0200

    LPD-35336 Update property to use the simple object name. With that, we will be able to filter the OSGi services using the x-schema-name property from the OpenAPI, otherwise it is not possible

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectActionLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectActionLocalServiceTest.java
@@ -2977,7 +2977,8 @@ public class ObjectActionLocalServiceTest {
 						com.liferay.object.rest.dto.v1_0.ObjectEntry.class.
 							getName(),
 						StringPool.POUND,
-						StringUtil.toLowerCase(_objectDefinition.getName())));
+						StringUtil.toLowerCase(
+							_objectDefinition.getShortName())));
 
 			objectEntryResource.setContextAcceptLanguage(
 				new AcceptLanguage() {
