5111a8a4821995f00a01e343838ce161eaecf097
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2024-10-24 12:25:56 +0200

    LPD-35336 buildRest

--- modules/util/portal-tools-rest-builder-test-client-js/src/main/resources/META-INF/resources/node/models/TestEntity.ts
+++ modules/util/portal-tools-rest-builder-test-client-js/src/main/resources/META-INF/resources/node/models/TestEntity.ts
@@ -7,16 +7,25 @@ import type { NestedTestEntity } from './NestedTestEntity';
  * https://www.schema.org/Document
  */
 export type TestEntity = {
+    customFields?: Record<string, any>;
+    expirationDate?: string;
     dateCreated?: string;
     dateModified?: string;
+    priority?: number;
+    folderId?: number;
+    groupId?: number;
+    creatorId?: number;
+    viewCount?: number;
     description?: string;
+    friendlyUrl?: string;
+    title?: string;
     documentId?: number;
     readonly id?: number;
     jsonProperty?: string;
     name?: string;
     nestedTestEntity?: NestedTestEntity;
     self?: string;
-    testEntities?: TestEntity;
+    testEntities?: Array<TestEntity>;
     type?: 'ChildTestEntity1' | 'ChildTestEntity2' | 'ChildTestEntity3';
 };
 
