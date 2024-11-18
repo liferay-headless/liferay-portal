901db8683cda7d4b1dae89676035965f4836a8f4
Vendel Toreki <vendel.toreki@liferay.com>
2024-11-18 15:00:44 +0100

    LPD-41431 Check the cleanup of BatchEngineExportTask and BatchEngineImportTaskError records as well. Check the counts to verify the deletion of the import tasks created implicitly by company creation.

--- modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/internal/model/listener/test/CompanyModelListenerTest.java
+++ modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/internal/model/listener/test/CompanyModelListenerTest.java
@@ -11,13 +11,17 @@ import com.liferay.batch.engine.BatchEngineTaskOperation;
 import com.liferay.batch.engine.constants.BatchEngineImportTaskConstants;
 import com.liferay.batch.engine.internal.test.BlogPosting;
 import com.liferay.batch.engine.model.BatchEngineImportTask;
+import com.liferay.batch.engine.model.BatchEngineImportTaskErrorTable;
+import com.liferay.batch.engine.service.BatchEngineExportTaskLocalService;
 import com.liferay.batch.engine.service.BatchEngineImportTaskErrorLocalService;
 import com.liferay.batch.engine.service.BatchEngineImportTaskLocalService;
+import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -50,12 +54,16 @@ public class CompanyModelListenerTest {
 
 		User user = UserTestUtil.getAdminUser(company.getCompanyId());
 
-		BatchEngineImportTask batchEngineImportTask = null;
-
 		try {
 			UserTestUtil.setUser(user);
 
-			batchEngineImportTask =
+			_batchEngineExportTaskLocalService.addBatchEngineExportTask(
+				null, company.getCompanyId(), user.getUserId(), null,
+				BlogPosting.class.getName(), "JSON",
+				BatchEngineTaskExecuteStatus.INITIAL.name(), null,
+				new HashMap<>(), null);
+
+			BatchEngineImportTask batchEngineImportTask =
 				_batchEngineImportTaskLocalService.addBatchEngineImportTask(
 					null, company.getCompanyId(), user.getUserId(), 10, null,
 					BlogPosting.class.getName(), new byte[0], "JSON",
@@ -64,20 +72,43 @@ public class CompanyModelListenerTest {
 						IMPORT_STRATEGY_ON_ERROR_FAIL,
 					BatchEngineTaskOperation.CREATE.name(), new HashMap<>(),
 					null);
+
+			_batchEngineImportTaskErrorLocalService.
+				addBatchEngineImportTaskError(
+					batchEngineImportTask.getCompanyId(),
+					batchEngineImportTask.getUserId(),
+					batchEngineImportTask.getBatchEngineImportTaskId(), null,
+					RandomTestUtil.randomInt(), "This is just an error");
 		}
 		finally {
 			_companyLocalService.deleteCompany(company);
 		}
 
-		Assert.assertNull(
-			_batchEngineImportTaskLocalService.fetchBatchEngineImportTask(
-				batchEngineImportTask.getBatchEngineImportTaskId()));
 		Assert.assertEquals(
 			0,
-			_batchEngineImportTaskErrorLocalService.
-				getBatchEngineImportTaskErrorsCount(
-					batchEngineImportTask.getBatchEngineImportTaskId()));
+			_batchEngineExportTaskLocalService.getBatchEngineExportTasksCount(
+				company.getCompanyId()));
+
+		Assert.assertEquals(
+			0,
+			_batchEngineImportTaskLocalService.getBatchEngineImportTasksCount(
+				company.getCompanyId()));
+
+		Assert.assertEquals(
+			0,
+			_batchEngineImportTaskErrorLocalService.dslQueryCount(
+				DSLQueryFactoryUtil.count(
+				).from(
+					BatchEngineImportTaskErrorTable.INSTANCE
+				).where(
+					BatchEngineImportTaskErrorTable.INSTANCE.companyId.eq(
+						company.getCompanyId())
+				)));
 	}
+
+	@Inject
+	private BatchEngineExportTaskLocalService
+		_batchEngineExportTaskLocalService;
 
 	@Inject
 	private BatchEngineImportTaskErrorLocalService
