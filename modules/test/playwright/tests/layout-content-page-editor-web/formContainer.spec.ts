37baa5f8ffe89b5a67ea9125a938230a00e1301d
Daniel Raposo <daniel.raposo@liferay.com>
2025-02-07 15:21:45 +0100

    LPD-48305 Remove unused imports

--- modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
@@ -5,9 +5,7 @@
 
 import {
 	ObjectDefinitionApi,
-	ObjectField,
 	ObjectFieldApi,
-	ObjectValidationRule,
 	ObjectValidationRuleApi,
 } from '@liferay/object-admin-rest-client-js';
 import {Page, expect, mergeTests} from '@playwright/test';
