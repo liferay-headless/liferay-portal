09f608650c71e6a46e6517104703f7b4789b15cc
Alejandro Tardín <alejandro.tardin@liferay.com>
2025-02-13 16:48:59 +0100

    LPD-48305 buildREST

--- modules/apps/object/object-admin-rest-client-js/src/node/model/models.ts
+++ modules/apps/object/object-admin-rest-client-js/src/node/model/models.ts
@@ -82,15 +82,15 @@ import { Status } from './status';
 
 /* tslint:disable:no-unused-variable */
 let primitives = [
-					"string",
-					"boolean",
-					"double",
-					"integer",
-					"long",
-					"float",
-					"number",
-					"any"
-				 ];
+	"string",
+	"boolean",
+	"double",
+	"integer",
+	"long",
+	"float",
+	"number",
+	"any"
+];
 
 let typeMap: {[index: string]: any} = {
 	"Facet": Facet,
