c06350b71406a39c722fc939a57d9ddcd46ce72c
Mauricio Valdivia <mauricio.valdivia@liferay.com>
2025-03-17 14:07:34 +0100

    LPD-49868 sf

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
@@ -2422,7 +2422,8 @@ public class StructuredContentResourceTest
 							JSONFactoryUtil.createJSONObject(
 								String.valueOf(
 									_randomStructuredContent(
-										LocaleUtil.getDefault())))).toString()
+										LocaleUtil.getDefault())))
+						).toString()
 					).getContent()));
 
 		Assert.assertEquals(1, jsonObject.getLong("processedItemsCount"));
