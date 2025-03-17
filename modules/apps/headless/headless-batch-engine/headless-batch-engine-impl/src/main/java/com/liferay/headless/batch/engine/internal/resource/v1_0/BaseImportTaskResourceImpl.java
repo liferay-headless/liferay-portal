8c96051c3809cd1f7aef0bec1c8919aff3deff28
Mauricio Valdivia <mauricio.valdivia@liferay.com>
2025-03-17 11:38:47 +0100

    LPD-49868 gw buildRest

--- modules/apps/headless/headless-batch-engine/headless-batch-engine-impl/src/main/java/com/liferay/headless/batch/engine/internal/resource/v1_0/BaseImportTaskResourceImpl.java
+++ modules/apps/headless/headless-batch-engine/headless-batch-engine-impl/src/main/java/com/liferay/headless/batch/engine/internal/resource/v1_0/BaseImportTaskResourceImpl.java
@@ -215,7 +215,7 @@ public abstract class BaseImportTaskResourceImpl
 			@io.swagger.v3.oas.annotations.Parameter(hidden = true)
 			@javax.ws.rs.QueryParam("taskItemDelegateName")
 			String taskItemDelegateName,
-			Object object)
+			String string)
 		throws Exception {
 
 		return new ImportTask();
@@ -376,7 +376,7 @@ public abstract class BaseImportTaskResourceImpl
 			@io.swagger.v3.oas.annotations.Parameter(hidden = true)
 			@javax.ws.rs.QueryParam("taskItemDelegateName")
 			String taskItemDelegateName,
-			Object object)
+			String string)
 		throws Exception {
 
 		return new ImportTask();
@@ -544,7 +544,7 @@ public abstract class BaseImportTaskResourceImpl
 			@io.swagger.v3.oas.annotations.Parameter(hidden = true)
 			@javax.ws.rs.QueryParam("updateStrategy")
 			String updateStrategy,
-			Object object)
+			String string)
 		throws Exception {
 
 		return new ImportTask();
