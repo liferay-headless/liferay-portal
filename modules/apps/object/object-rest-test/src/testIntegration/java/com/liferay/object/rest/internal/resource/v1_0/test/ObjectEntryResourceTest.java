48553841e497a1d23d30bda6256f37c8567ee354
Carlos Correa <carlos.correa@liferay.com>
2025-04-23 09:12:53 +0200

    LPD-53245 Remove flakiness, the round method with the MathContext takes the N most significative numbers, but not only decimals, so I replace with setScale

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -188,6 +188,7 @@ import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.math.MathContext;
 
+import java.math.RoundingMode;
 import java.sql.Timestamp;
 
 import java.text.DateFormat;
@@ -7604,7 +7605,7 @@ public class ObjectEntryResourceTest {
 			)
 		).put(
 			_OBJECT_FIELD_NAME_PRECISION_DECIMAL,
-			randomBigDecimal.round(new MathContext(16))
+			randomBigDecimal.setScale(16, RoundingMode.HALF_UP)
 		).put(
 			_OBJECT_FIELD_NAME_RICH_TEXT, randomString2
 		).put(
@@ -7758,7 +7759,7 @@ public class ObjectEntryResourceTest {
 
 		expectedJSONObject = _cloneJSONObject(
 			expectedJSONObject, _OBJECT_FIELD_NAME_PRECISION_DECIMAL,
-			bigDecimal.round(new MathContext(17)));
+			bigDecimal.setScale(16, RoundingMode.HALF_UP));
 
 		_testPatchCustomObjectEntry(
 			expectedJSONObject, _OBJECT_FIELD_NAME_PRECISION_DECIMAL,
@@ -10141,7 +10142,7 @@ public class ObjectEntryResourceTest {
 				)
 			).put(
 				_OBJECT_FIELD_NAME_PRECISION_DECIMAL,
-				randomBigDecimal.round(new MathContext(16))
+				randomBigDecimal.setScale(16, RoundingMode.HALF_UP)
 			).put(
 				_OBJECT_FIELD_NAME_RICH_TEXT, randomString2
 			).put(
@@ -10962,7 +10963,7 @@ public class ObjectEntryResourceTest {
 				)
 			).put(
 				_OBJECT_FIELD_NAME_PRECISION_DECIMAL,
-				randomBigDecimal.round(new MathContext(16))
+				randomBigDecimal.setScale(16, RoundingMode.HALF_UP)
 			).put(
 				_OBJECT_FIELD_NAME_RICH_TEXT, randomString2
 			).put(
