d2174c69cdd93434b6fa468ec630d30b602b3412
Jaime Leon <jaime.leon@liferay.com>
2025-05-14 17:10:18 +0200

    LPD-55081 Create import button in react

---
+++ modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/js/components/button/ImportButton.tsx
@@ -0,0 +1,46 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayButton from '@clayui/button';
+import React, {useState} from 'react';
+
+import {ModalConfirmImport} from '../modal/ModalConfirmImport';
+
+export default function ImportButton({
+	checkAnyObjectEntrySelectedFnName,
+	copyAsNewCheckboxId,
+	deletePortletDataBeforeImportingCheckboxId,
+	handleSubmitFnName,
+	mirrorWithOverwritingCheckboxId,
+}: {
+	checkAnyObjectEntrySelectedFnName: string;
+	copyAsNewCheckboxId: string;
+	deletePortletDataBeforeImportingCheckboxId: string;
+	handleSubmitFnName: string;
+	mirrorWithOverwritingCheckboxId: string;
+}) {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleSubmit = () => {
+		
+	};
+
+	return (
+		<div>
+			<ClayButton.Group spaced>
+				<ClayButton onClick={handleSubmit}>
+					{Liferay.Language.get('Import')}
+				</ClayButton>
+			</ClayButton.Group>
+
+			{isOpen && (
+				<ModalConfirmImport
+					handleOnClose={() => setIsOpen(false)}
+					handleSubmitFnName={handleSubmitFnName}
+				/>
+			)}
+		</div>
+	);
+}
