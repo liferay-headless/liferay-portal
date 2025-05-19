1407c7e2a8800bbdf24655d268c675551e386c16
Álvaro Leal <alvaro.leal@liferay.com>
2025-05-19 11:28:35 -0300

    LPD-54895 Add  users and organitazions page locators

--- modules/test/playwright/pages/users-admin-web/UsersAndOrganizationsPage.ts
+++ modules/test/playwright/pages/users-admin-web/UsersAndOrganizationsPage.ts
@@ -160,6 +160,11 @@ export class UsersAndOrganizationsPage {
 	readonly userPersonalMenuButton: Locator;
 	readonly usersTable: Locator;
 	readonly usersTableCell: (userName: string) => Locator;
+	readonly userPreferencesButton: Locator;
+	readonly displaySettingsButton: Locator;
+	readonly timeZoneSelect: Locator;
+	readonly saveTimeZoneButton: Locator;
+	readonly usersAndOrganizationsButton: Locator;
 
 	constructor(page: Page) {
 		this.activateButton = page.getByRole('button', {name: 'Activate'});
@@ -580,6 +585,14 @@ export class UsersAndOrganizationsPage {
 				name: userName,
 			});
 		};
+		this.userPreferencesButton = page.getByRole('link', {
+			name: 'Preferences',
+		});
+		this.displaySettingsButton = page.getByRole('link', {
+			name: 'Display Settings',
+		});
+		this.timeZoneSelect = page.getByLabel('Time Zone');
+		this.saveTimeZoneButton = page.getByRole('button', {name: 'Save'});
 	}
 
 	async activateUsers(userNames: string[]) {
