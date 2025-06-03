346fed372c2b5eaea7b23e3727d1a3878ac8cb28
Daniel Raposo <daniel.raposo@liferay.com>
2025-06-03 20:39:01 +0200

    LPD-46121 buildREST

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/BasePageSpecificationResourceTestCase.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/BasePageSpecificationResourceTestCase.java
@@ -1052,6 +1052,11 @@ public abstract class BasePageSpecificationResourceTestCase {
 			"This method needs to be implemented");
 	}
 
+	@Test
+	public void testBatchEngineDeleteImportTask() throws Exception {
+		Assert.assertTrue(true);
+	}
+
 	protected void assertContains(
 		PageSpecification pageSpecification,
 		List<PageSpecification> pageSpecifications) {
