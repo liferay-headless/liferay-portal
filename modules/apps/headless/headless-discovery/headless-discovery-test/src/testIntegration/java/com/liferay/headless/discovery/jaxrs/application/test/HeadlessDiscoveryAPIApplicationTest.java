11bef81ea26cc6debc330ee3655d099833989716
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2025-07-07 17:21:23 +0200

    LPD-59421 Add test to reproduce the issue

---
+++ modules/apps/headless/headless-discovery/headless-discovery-test/src/testIntegration/java/com/liferay/headless/discovery/jaxrs/application/test/HeadlessDiscoveryAPIApplicationTest.java
@@ -0,0 +1,60 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.discovery.jaxrs.application.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.TestInfo;
+import com.liferay.portal.kernel.test.rule.DataGuard;
+import com.liferay.portal.kernel.test.util.HTTPTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.Http;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Magdalena Jedraszak
+ */
+@DataGuard(scope = DataGuard.Scope.METHOD)
+@RunWith(Arquillian.class)
+public class HeadlessDiscoveryAPIApplicationTest {
+
+	@Test
+	@TestInfo("LPD-59421")
+	public void test() throws Exception {
+		Map<String, String> headers = HashMapBuilder.put(
+			"Accept", "text/html"
+		).put(
+			"Referer", "http://attacker.local:8087"
+		).build();
+
+		Assert.assertEquals(
+			403,
+			HTTPTestUtil.invokeToHttpCode(
+				null,
+				"api?endpoint=http://attacker.local:8087/o/batch-planner/v1.0" +
+					"/openapi.json",
+				headers, Http.Method.GET));
+
+		headers = HashMapBuilder.put(
+			"Accept", "text/html"
+		).put(
+			"Referer", "http://localhost:8080"
+		).build();
+
+		Assert.assertEquals(
+			200,
+			HTTPTestUtil.invokeToHttpCode(
+				null,
+				"api?endpoint=http://localhost:8080/o/batch-planner/v1.0" +
+					"/openapi.json",
+				headers, Http.Method.GET));
+	}
+
+}
