2399c7e8fb5fc8b19332ef1a08711862058df168
Carlos Correa <carlos.correa@liferay.com>
2025-08-01 10:28:59 +0200

    LPD-61067 Create class to use it to simulate a Batch Import from the Export-Import

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/exportimport/data/handler/BatchEnginePortletDataHandler.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/exportimport/data/handler/BatchEnginePortletDataHandler.java
@@ -98,6 +98,9 @@ public class BatchEnginePortletDataHandler extends BasePortletDataHandler {
 		}
 
 		setEmptyControlsAllowed(true);
+
+		setPortletId(
+			exportImportVulcanBatchEngineTaskItemDelegate.getPortletId());
 	}
 
 	public void exportDeletionSystemEvents(
--- modules/apps/export-import/export-import-test-util/src/main/java/com/liferay/exportimport/test/util/ExportImportTestUtil.java
+++ modules/apps/export-import/export-import-test-util/src/main/java/com/liferay/exportimport/test/util/ExportImportTestUtil.java
@@ -5,18 +5,53 @@
 
 package com.liferay.exportimport.test.util;
 
+import com.liferay.batch.engine.BatchEngineImportTaskExecutor;
+import com.liferay.batch.engine.BatchEngineTaskContentType;
+import com.liferay.batch.engine.BatchEngineTaskExecuteStatus;
+import com.liferay.batch.engine.BatchEngineTaskOperation;
+import com.liferay.batch.engine.constants.BatchEngineImportTaskConstants;
+import com.liferay.batch.engine.constants.CreateStrategy;
+import com.liferay.batch.engine.model.BatchEngineImportTask;
+import com.liferay.batch.engine.service.BatchEngineImportTaskLocalService;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.PortletDataContextFactoryUtil;
+import com.liferay.exportimport.kernel.lar.PortletDataHandler;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.petra.function.UnsafeRunnable;
+import com.liferay.petra.io.StreamUtil;
+import com.liferay.petra.io.unsync.UnsyncByteArrayOutputStream;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.lazy.referencing.LazyReferencingThreadLocal;
+import com.liferay.portal.kernel.module.service.Snapshot;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
+
+import java.io.ByteArrayInputStream;
+import java.io.Serializable;
+
+import java.nio.charset.StandardCharsets;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Máté Thurzó
@@ -130,6 +165,68 @@ public class ExportImportTestUtil {
 			companyId, groupId, new HashMap<String, String[]>());
 	}
 
+	public static Status importJSONArray(
+			long companyId, JSONArray jsonArray, String portletId)
+		throws Exception {
+
+		BatchEngineImportTaskLocalService batchEngineImportTaskLocalService =
+			_batchEngineImportTaskLocalServiceSnapshot.get();
+
+		String[] nameParts = StringUtil.split(
+			_serviceTrackerMap.getService(portletId), StringPool.POUND);
+
+		String taskItemDelegateName = nameParts[1];
+
+		if (Validator.isNull(taskItemDelegateName)) {
+			taskItemDelegateName = null;
+		}
+
+		BatchEngineImportTask batchEngineImportTask =
+			batchEngineImportTaskLocalService.addBatchEngineImportTask(
+				RandomTestUtil.randomString(), companyId,
+				TestPropsValues.getUserId(), 100, null, nameParts[0],
+				_toByteArray(jsonArray.toString()),
+				BatchEngineTaskContentType.JSON.toString(),
+				BatchEngineTaskExecuteStatus.INITIAL.name(), new HashMap<>(),
+				BatchEngineImportTaskConstants.
+					IMPORT_STRATEGY_ON_ERROR_CONTINUE,
+				BatchEngineTaskOperation.CREATE.name(),
+				HashMapBuilder.<String, Serializable>put(
+					"createStrategy", CreateStrategy.UPSERT.getDBOperation()
+				).build(),
+				taskItemDelegateName);
+
+		BatchEngineImportTaskExecutor batchEngineImportTaskExecutor =
+			_batchEngineImportTaskExecutorSnapshot.get();
+
+		try (SafeCloseable safeCloseable =
+				LazyReferencingThreadLocal.setEnabledWithSafeCloseable(true)) {
+
+			batchEngineImportTaskExecutor.execute(batchEngineImportTask);
+		}
+
+		batchEngineImportTask =
+			batchEngineImportTaskLocalService.getBatchEngineImportTask(
+				batchEngineImportTask.getBatchEngineImportTaskId());
+
+		BatchEngineTaskExecuteStatus batchEngineTaskExecuteStatus =
+			BatchEngineTaskExecuteStatus.valueOf(
+				batchEngineImportTask.getExecuteStatus());
+
+		if (BatchEngineTaskExecuteStatus.COMPLETED ==
+				batchEngineTaskExecuteStatus) {
+
+			return Status.SUCCESS;
+		}
+		else if (BatchEngineTaskExecuteStatus.FAILED ==
+					batchEngineTaskExecuteStatus) {
+
+			return Status.FAILURE;
+		}
+
+		throw new IllegalStateException();
+	}
+
 	public static void retryAssert(
 			long pause, TimeUnit pauseTimeUnit, long timeout,
 			TimeUnit timeoutTimeUnit, UnsafeRunnable<Exception> unsafeRunnable)
@@ -152,6 +249,87 @@ public class ExportImportTestUtil {
 
 			Thread.sleep(pauseTimeUnit.toMillis(pause));
 		}
+	}
+
+	public enum Status {
+
+		FAILURE, SUCCESS
+
+	}
+
+	private static byte[] _toByteArray(String content) throws Exception {
+		UnsyncByteArrayOutputStream unsyncByteArrayOutputStream =
+			new UnsyncByteArrayOutputStream();
+
+		try (ZipOutputStream zipOutputStream = new ZipOutputStream(
+				unsyncByteArrayOutputStream)) {
+
+			ZipEntry zipEntry = new ZipEntry("fileName");
+
+			zipOutputStream.putNextEntry(zipEntry);
+
+			StreamUtil.transfer(
+				new ByteArrayInputStream(
+					content.getBytes(StandardCharsets.UTF_8)),
+				zipOutputStream, false);
+		}
+
+		return unsyncByteArrayOutputStream.toByteArray();
+	}
+
+	private static final Snapshot<BatchEngineImportTaskExecutor>
+		_batchEngineImportTaskExecutorSnapshot = new Snapshot<>(
+			ExportImportTestUtil.class, BatchEngineImportTaskExecutor.class);
+	private static final Snapshot<BatchEngineImportTaskLocalService>
+		_batchEngineImportTaskLocalServiceSnapshot = new Snapshot<>(
+			ExportImportTestUtil.class,
+			BatchEngineImportTaskLocalService.class);
+	private static final ServiceTrackerMap<String, String> _serviceTrackerMap;
+
+	static {
+		Bundle bundle = FrameworkUtil.getBundle(ExportImportTestUtil.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, PortletDataHandler.class, null,
+			(serviceReference, emitter) -> {
+				PortletDataHandler portletDataHandler =
+					bundleContext.getService(serviceReference);
+
+				if (portletDataHandler.isBatch()) {
+					emitter.emit(portletDataHandler.getPortletId());
+				}
+
+				bundleContext.ungetService(serviceReference);
+			},
+			new ServiceTrackerCustomizer<PortletDataHandler, String>() {
+
+				@Override
+				public String addingService(
+					ServiceReference<PortletDataHandler> serviceReference) {
+
+					PortletDataHandler portletDataHandler =
+						bundleContext.getService(serviceReference);
+
+					return portletDataHandler.getName();
+				}
+
+				@Override
+				public void modifiedService(
+					ServiceReference<PortletDataHandler> serviceReference,
+					String string) {
+				}
+
+				@Override
+				public void removedService(
+					ServiceReference<PortletDataHandler> serviceReference,
+					String string) {
+
+					bundleContext.ungetService(serviceReference);
+				}
+
+			});
 	}
 
 }
