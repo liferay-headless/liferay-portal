507d30cdd320c94e3468d01fff0aaac627671ed9
Carlos Correa <carlos.correa@liferay.com>
2025-09-10 14:56:44 +0200

    LPD-58307 Move to the exception.handler package

--- modules/apps/export-import/export-import-report-service/src/main/java/com/liferay/exportimport/report/internal/exception/handler/ImportStagedModelExceptionHandlerImpl.java
+++ modules/apps/export-import/export-import-report-service/src/main/java/com/liferay/exportimport/report/internal/exception/handler/ImportStagedModelExceptionHandlerImpl.java
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.exportimport.report.service.impl.handler;
+package com.liferay.exportimport.report.internal.exception.handler;
 
-import com.liferay.exportimport.kernel.exception.ImportStagedModelExceptionHandler;
+import com.liferay.exportimport.kernel.exception.handler.ImportStagedModelExceptionHandler;
 import com.liferay.exportimport.kernel.lar.ExportImportClassedModelUtil;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.PortletDataException;
--- modules/apps/export-import/export-import-report-test/src/testIntegration/java/com/liferay/exportimport/report/internal/exception/handler/test/ImportStagedModelExceptionHandlerTest.java
+++ modules/apps/export-import/export-import-report-test/src/testIntegration/java/com/liferay/exportimport/report/internal/exception/handler/test/ImportStagedModelExceptionHandlerTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.exportimport.report.service.test;
+package com.liferay.exportimport.report.internal.exception.handler.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.expando.kernel.model.ExpandoBridge;
