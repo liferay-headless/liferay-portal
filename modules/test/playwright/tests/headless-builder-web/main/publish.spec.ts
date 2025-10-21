1d44768233a1818d48433df636113badf88b3aad
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2025-10-21 09:57:20 +0200

    LPD-68786 Refactor

--- modules/test/playwright/tests/headless-builder-web/main/publish.spec.ts
+++ modules/test/playwright/tests/headless-builder-web/main/publish.spec.ts
@@ -17,93 +17,76 @@ export const test = mergeTests(
 	loginTest()
 );
 
-test('Can get  updated title in response after publish', async ({
-	apiHelpers,
-	applicationPage,
-	headlessBuilderPage,
-	page,
-}) => {
-	const application = await apiHelpers.objectEntry.postObjectEntry(
-		{
-			apiApplicationToAPISchemas: [
-				{
-					description: 'API Application Schema',
-					externalReferenceCode: 'api-application-schema',
-					mainObjectDefinitionERC: 'L_API_APPLICATION',
-					name: 'API Application Schema',
-				},
-			],
-			applicationStatus: 'unpublished',
-			baseURL: 'basic-application',
-			description: 'Test API Application',
-			externalReferenceCode: 'basic-application',
-			title: 'Basic application',
-		},
-		'headless-builder/applications'
-	);
+test.describe('Headless Builder - API Application', () => {
+	let application;
 
-	apiHelpers.data.push({id: application.id, type: 'apiApplication'});
+	test.beforeEach(async ({apiHelpers, headlessBuilderPage, page}) => {
+		application = await apiHelpers.objectEntry.postObjectEntry(
+			{
+				apiApplicationToAPISchemas: [
+					{
+						description: 'API Application Schema',
+						externalReferenceCode: 'api-application-schema',
+						mainObjectDefinitionERC: 'L_API_APPLICATION',
+						name: 'API Application Schema',
+					},
+				],
+				applicationStatus: 'unpublished',
+				baseURL: 'basic-application',
+				description: 'Test API Application',
+				externalReferenceCode: 'basic-application',
+				title: 'Basic application',
+			},
+			'headless-builder/applications'
+		);
 
-	await headlessBuilderPage.goto();
-	await headlessBuilderPage.openApplicationActions(application.title);
-	await page.getByRole('menuitem', {name: 'Edit'}).click();
+		apiHelpers.data.push({id: application.id, type: 'apiApplication'});
 
-	await applicationPage.applicationTitleTextBox.fill(
-		`${application.title} 1`
-	);
-	await applicationPage.publishButton.click();
-	await expect(page.getByText('API application was published')).toBeVisible();
+		await headlessBuilderPage.goto();
+		await headlessBuilderPage.openApplicationActions(application.title);
+		await page.getByRole('menuitem', {name: 'Edit'}).click();
+	});
 
-	expect(
-		(
+	test('Can get updated title in response after publish', async ({
+		apiHelpers,
+		applicationPage,
+		page,
+	}) => {
+		await applicationPage.applicationTitleTextBox.fill(
+			`${application.title} 1`
+		);
+		await applicationPage.publishButton.click();
+
+		await expect(
+			page.getByText('API application was published')
+		).toBeVisible();
+
+		const updatedApp =
 			await apiHelpers.objectEntry.getObjectEntryByExternalReferenceCode({
 				applicationName: 'headless-builder/applications',
 				externalReferenceCode: application.externalReferenceCode,
-			})
-		).title
-	).toEqual(`${application.title} 1`);
-});
+			});
 
-test('Can see cancel and publish buttons enabled after publish application', async ({
-	apiHelpers,
-	applicationPage,
-	headlessBuilderPage,
-	page,
-}) => {
-	const application = await apiHelpers.objectEntry.postObjectEntry(
-		{
-			apiApplicationToAPISchemas: [
-				{
-					description: 'API Application Schema',
-					externalReferenceCode: 'api-application-schema',
-					mainObjectDefinitionERC: 'L_API_APPLICATION',
-					name: 'API Application Schema',
-				},
-			],
-			applicationStatus: 'unpublished',
-			baseURL: 'basic-application',
-			description: 'Test API Application',
-			externalReferenceCode: 'basic-application',
-			title: 'Basic application',
-		},
-		'headless-builder/applications'
-	);
+		expect(updatedApp.title).toEqual(`${application.title} 1`);
+	});
 
-	apiHelpers.data.push({id: application.id, type: 'apiApplication'});
+	test('Can see cancel and publish buttons enabled after publish application', async ({
+		applicationPage,
+		page,
+	}) => {
+		await applicationPage.publishButton.click();
 
-	await headlessBuilderPage.goto();
-	await headlessBuilderPage.openApplicationActions(application.title);
-	await page.getByRole('menuitem', {name: 'Edit'}).click();
-	await applicationPage.publishButton.click();
+		await expect(
+			page.getByText('API application was published')
+		).toBeVisible();
+		await expect(
+			page.getByText('API application was published')
+		).not.toBeVisible();
 
-	await expect(page.getByText('API application was published')).toBeVisible();
-	await expect(
-		page.getByText('API application was published')
-	).not.toBeVisible();
+		await expect(applicationPage.cancelButton).toBeEnabled();
+		await expect(applicationPage.cancelButton).toBeVisible();
 
-	await expect(applicationPage.cancelButton).toBeEnabled();
-	await expect(applicationPage.cancelButton).toBeVisible();
-
-	await expect(applicationPage.publishButton).toBeEnabled();
-	await expect(applicationPage.publishButton).toBeVisible();
+		await expect(applicationPage.publishButton).toBeEnabled();
+		await expect(applicationPage.publishButton).toBeVisible();
+	});
 });
