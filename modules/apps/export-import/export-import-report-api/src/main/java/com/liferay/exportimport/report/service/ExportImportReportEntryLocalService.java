6a1d9f225d239efe48416074781ef704d547bc65
Álvaro Saugar <alvaro.saugar@liferay.com>
2025-10-28 17:08:16 +0100

    LPD-66994 buildService

--- modules/apps/export-import/export-import-report-api/src/main/java/com/liferay/exportimport/report/service/ExportImportReportEntryLocalService.java
+++ modules/apps/export-import/export-import-report-api/src/main/java/com/liferay/exportimport/report/service/ExportImportReportEntryLocalService.java
@@ -59,7 +59,6 @@ public interface ExportImportReportEntryLocalService
 		int origin, String scope, String scopeKey);
 
 	@Indexable(type = IndexableType.REINDEX)
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public ExportImportReportEntry addErrorExportImportReportEntry(
 		long groupId, long companyId, String classExternalReferenceCode,
 		long classNameId, long classPK, long exportImportConfigurationId,
