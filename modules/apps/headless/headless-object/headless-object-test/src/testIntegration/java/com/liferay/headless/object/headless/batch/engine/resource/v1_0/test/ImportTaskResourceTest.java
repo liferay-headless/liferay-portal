bf43365d739e0a60230a133fe0b1583fb0121ac4
Alberto Moreno <alberto.moreno@liferay.com>
2026-01-21 10:56:39 +0100

    LPD-74573 Do not inherit base class since you do not need all of the base tests

--- modules/apps/headless/headless-object/headless-object-test/src/testIntegration/java/com/liferay/headless/object/headless/batch/engine/resource/v1_0/test/ImportTaskResourceTest.java
+++ modules/apps/headless/headless-object/headless-object-test/src/testIntegration/java/com/liferay/headless/object/headless/batch/engine/resource/v1_0/test/ImportTaskResourceTest.java
@@ -9,14 +9,29 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.headless.batch.engine.client.dto.v1_0.ImportTask;
 import com.liferay.headless.batch.engine.client.resource.v1_0.ImportTaskResource;
 import com.liferay.headless.object.client.dto.v1_0.ObjectEntryFolder;
-import com.liferay.headless.object.resource.v1_0.test.BaseObjectEntryFolderResourceTestCase;
+import com.liferay.headless.object.client.resource.v1_0.ObjectEntryFolderResource;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PropsValues;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.FeatureFlag;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -28,8 +43,53 @@ import org.skyscreamer.jsonassert.JSONCompareMode;
  */
 @FeatureFlag("LPD-17564")
 @RunWith(Arquillian.class)
-public class ImportTaskResourceTest
-	extends BaseObjectEntryFolderResourceTestCase {
+public class ImportTaskResourceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_testGroup = GroupTestUtil.addGroup();
+
+		_testCompany = CompanyLocalServiceUtil.getCompany(
+			_testGroup.getCompanyId());
+
+		_testCompanyAdminUser = UserTestUtil.getAdminUser(
+			_testCompany.getCompanyId());
+
+		_importTaskResource = ImportTaskResource.builder(
+		).authentication(
+			"test@liferay.com", PropsValues.DEFAULT_ADMIN_PASSWORD
+		).endpoint(
+			_testCompany.getVirtualHostname(), 8080, "http"
+		).locale(
+			LocaleUtil.getDefault()
+		).parameter(
+			"siteExternalReferenceCode", _testGroup.getExternalReferenceCode()
+		).parameter(
+			"taskItemDelegateName", "depot-object-entry-folder"
+		).build();
+
+		_objectEntryFolderResource = ObjectEntryFolderResource.builder(
+		).authentication(
+			_testCompanyAdminUser.getEmailAddress(),
+			PropsValues.DEFAULT_ADMIN_PASSWORD
+		).endpoint(
+			_testCompany.getVirtualHostname(), 8080, "http"
+		).locale(
+			LocaleUtil.getDefault()
+		).build();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		GroupTestUtil.deleteGroup(_testGroup);
+	}
 
 	@Test
 	public void testPostImportTask() throws Exception {
@@ -88,32 +148,47 @@ public class ImportTaskResourceTest
 			String createStrategy, JSONObject jsonObject)
 		throws Exception {
 
-		ImportTaskResource importTaskResource = ImportTaskResource.builder(
-		).authentication(
-			"test@liferay.com", PropsValues.DEFAULT_ADMIN_PASSWORD
-		).endpoint(
-			testCompany.getVirtualHostname(), 8080, "http"
-		).locale(
-			LocaleUtil.getDefault()
-		).parameter(
-			"siteExternalReferenceCode", testGroup.getExternalReferenceCode()
-		).parameter(
-			"taskItemDelegateName", "depot-object-entry-folder"
-		).build();
-
-		ImportTask importTask = importTaskResource.postImportTask(
+		ImportTask importTask = _importTaskResource.postImportTask(
 			"com.liferay.headless.object.dto.v1_0.ObjectEntryFolder", null,
 			null, null, createStrategy, null, null, null, null,
 			JSONUtil.putAll(
 				jsonObject
 			).toString());
 
-		waitForFinish("COMPLETED", JSONUtil.put("id", importTask.getId()));
+		_waitForFinish("COMPLETED", JSONUtil.put("id", importTask.getId()));
 
-		return objectEntryFolderResource.
+		return _objectEntryFolderResource.
 			getScopeScopeKeyObjectEntryFolderByExternalReferenceCode(
-				String.valueOf(testGroup.getGroupId()),
+				String.valueOf(_testGroup.getGroupId()),
 				jsonObject.getString("externalReferenceCode"));
 	}
+
+	private JSONObject _waitForFinish(
+			String expectedExecuteStatus, JSONObject jsonObject)
+		throws Exception {
+
+		while (true) {
+			ImportTask importTask = _importTaskResource.getImportTask(
+				jsonObject.getLong("id"));
+
+			ImportTask.ExecuteStatus executeStatus =
+				importTask.getExecuteStatus();
+
+			if (StringUtil.equals(executeStatus.getValue(), "COMPLETED") ||
+				StringUtil.equals(executeStatus.getValue(), "FAILED")) {
+
+				Assert.assertEquals(
+					expectedExecuteStatus, executeStatus.getValue());
+
+				return jsonObject;
+			}
+		}
+	}
+
+	private ImportTaskResource _importTaskResource;
+	private ObjectEntryFolderResource _objectEntryFolderResource;
+	private Company _testCompany;
+	private User _testCompanyAdminUser;
+	private Group _testGroup;
 
 }
