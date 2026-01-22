100daade0809b346a5a014289da3a6e0b1c8372c
Petteri Karttunen <petteri.karttunen@liferay.com>
2026-01-22 11:18:30 +0200

    LPD-76860 export-import-service: Like theme, always update the color scheme as well

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/controller/LayoutImportController.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/controller/LayoutImportController.java
@@ -930,6 +930,9 @@ public class LayoutImportController implements ImportController {
 					publicLayoutSet.setThemeId(
 						GetterUtil.getString(
 							headerElement.attributeValue("theme-id")));
+					publicLayoutSet.setColorSchemeId(
+						GetterUtil.getString(
+							headerElement.attributeValue("color-scheme-id")));
 
 					_layoutSetLocalService.updateLayoutSet(publicLayoutSet);
 				}
@@ -942,6 +945,9 @@ public class LayoutImportController implements ImportController {
 					privateLayoutSet.setThemeId(
 						GetterUtil.getString(
 							headerElement.attributeValue("theme-id")));
+					privateLayoutSet.setColorSchemeId(
+						GetterUtil.getString(
+							headerElement.attributeValue("color-scheme-id")));
 
 					_layoutSetLocalService.updateLayoutSet(privateLayoutSet);
 				}
@@ -958,6 +964,9 @@ public class LayoutImportController implements ImportController {
 					layoutSet.setThemeId(
 						GetterUtil.getString(
 							headerElement.attributeValue("theme-id")));
+					layoutSet.setColorSchemeId(
+						GetterUtil.getString(
+							headerElement.attributeValue("color-scheme-id")));
 
 					_layoutSetLocalService.updateLayoutSet(layoutSet);
 				}
