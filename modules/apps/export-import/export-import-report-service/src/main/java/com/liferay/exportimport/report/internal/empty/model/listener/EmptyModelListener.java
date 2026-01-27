d5d27689962ac7cdd46b7e8eb8caec8d2d88fc4a
Alberto Moreno <alberto.moreno@liferay.com>
2026-01-27 09:11:57 +0100

    LPD-76331 Add abstract model listener which should be extended by those models that can be empty

---
+++ modules/apps/export-import/export-import-report-service/src/main/java/com/liferay/exportimport/report/internal/empty/model/listener/EmptyModelListener.java
@@ -0,0 +1,60 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.exportimport.report.internal.empty.model.listener;
+
+import com.liferay.exportimport.kernel.lar.ExportImportThreadLocal;
+import com.liferay.exportimport.report.model.ExportImportReportEntry;
+import com.liferay.exportimport.report.service.ExportImportReportEntryLocalService;
+import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.model.BaseModelListener;
+
+/**
+ * @author Alberto Javier Moreno Lage
+ */
+public abstract class EmptyModelListener<T extends BaseModel<T>>
+	extends BaseModelListener<T> {
+
+	public void solveEmptyExportImportReportEntry(
+		long groupId, long companyId, String classExternalReferenceCode,
+		long classNameId) {
+
+		ExportImportReportEntry exportImportReportEntry =
+			_exportImportReportEntryLocalService.
+				fetchEmptyExportImportReportEntryByG_C_C_C(
+					groupId, companyId, classExternalReferenceCode,
+					classNameId);
+
+		if (exportImportReportEntry == null) {
+			return;
+		}
+
+		if (ExportImportThreadLocal.isImportInProcess() &&
+			(ExportImportThreadLocal.getExportImportConfigurationId() ==
+				exportImportReportEntry.getExportImportConfigurationId())) {
+
+			_exportImportReportEntryLocalService.deleteExportImportReportEntry(
+				exportImportReportEntry);
+		}
+		else {
+			exportImportReportEntry.setStatus(1);
+
+			_exportImportReportEntryLocalService.updateExportImportReportEntry(
+				exportImportReportEntry);
+		}
+	}
+
+	protected EmptyModelListener(
+		ExportImportReportEntryLocalService
+			exportImportReportEntryLocalService) {
+
+		_exportImportReportEntryLocalService =
+			exportImportReportEntryLocalService;
+	}
+
+	private final ExportImportReportEntryLocalService
+		_exportImportReportEntryLocalService;
+
+}
