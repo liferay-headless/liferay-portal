46d9b34f80dc0a6db11fc3ab0c137619eabfb848
Daniel Raposo <daniel.raposo@liferay.com>
2026-01-31 12:05:53 +0100

    LPD-77963 Add missingPortlet with minimum info (displayName, exportModelCount, portletId) if the portletDataHandler is missing

--- modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
@@ -25,6 +25,14 @@ import java.util.Map;
  */
 public class ObjectDefinitionUtil {
 
+	public static String getClassName(String portletId) {
+		return StringUtil.replaceFirst(
+			portletId,
+			ObjectPortletKeys.OBJECT_DEFINITIONS + StringPool.UNDERLINE,
+			ObjectDefinitionConstants.
+				CLASS_NAME_PREFIX_CUSTOM_OBJECT_DEFINITION);
+	}
+
 	public static String getModifiableSystemObjectDefinitionRESTContextPath(
 		String name) {
 
