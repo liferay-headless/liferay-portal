cd2c25ad5fb462459c108dab8b95a79562829bf0
Petteri Karttunen <petteri.karttunen@liferay.com>
2026-02-06 15:22:36 +0200

    LPD-75738 portal-vulcan-api: Add shared utility for building filter parameter when Changesets are involved

---
+++ modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/util/ParameterUtil.java
@@ -0,0 +1,79 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.vulcan.util;
+
+import com.liferay.changeset.model.ChangesetEntry;
+import com.liferay.changeset.service.ChangesetEntryLocalServiceUtil;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.petra.function.UnsafeFunction;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.ExternalReferenceCodeModel;
+import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Petteri Karttunen
+ */
+public class ParameterUtil {
+
+	public static String buildFilterParameterFromChangeset(
+		UnsafeFunction<Long, ExternalReferenceCodeModel, PortalException>
+			unsafeFunction,
+		String modelClassName, PortletDataContext portletDataContext) {
+
+		long changesetCollectionId = MapUtil.getLong(
+			portletDataContext.getParameterMap(), "changesetCollectionId");
+
+		if (changesetCollectionId == 0) {
+			return null;
+		}
+
+		Set<String> externalReferenceCodes = new HashSet<>();
+
+		externalReferenceCodes.add("");
+
+		List<ChangesetEntry> changesetEntries =
+			ChangesetEntryLocalServiceUtil.getChangesetEntries(
+				changesetCollectionId,
+				ClassNameLocalServiceUtil.getClassNameId(modelClassName));
+
+		for (ChangesetEntry changesetEntry : changesetEntries) {
+			try {
+				ExternalReferenceCodeModel externalReferenceCodeModel =
+					unsafeFunction.apply(changesetEntry.getClassPK());
+
+				if (externalReferenceCodeModel != null) {
+					externalReferenceCodes.add(
+						externalReferenceCodeModel.getExternalReferenceCode());
+				}
+			}
+			catch (Exception exception) {
+				_log.error(exception);
+			}
+		}
+
+		return StringBundler.concat(
+			"externalReferenceCode in (",
+			StringUtil.merge(
+				TransformUtil.transform(
+					externalReferenceCodes,
+					layoutExternalReferenceCode ->
+						"'" + layoutExternalReferenceCode + "'")),
+			")");
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(ParameterUtil.class);
+
+}
