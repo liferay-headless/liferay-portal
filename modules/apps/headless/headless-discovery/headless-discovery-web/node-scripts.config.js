2608ed8662b047d7baede06bcb7c1d5f4e383846
Carlos Correa <carlos.correa@liferay.com>
2026-03-23 15:15:19 +0100

    LPD-81731 Configure esbuild for Monaco Editor (resolve imports, deduplicate @graphiql/react, bundle workers)

--- modules/apps/headless/headless-discovery/headless-discovery-web/node-scripts.config.js
+++ modules/apps/headless/headless-discovery/headless-discovery-web/node-scripts.config.js
@@ -10,17 +10,60 @@ const path = require('path');
 
 const outdir = path.resolve('build', 'node', 'packageRunBuild', 'resources');
 
+const monacoEditorPath = path.dirname(
+	require.resolve('monaco-editor/package.json')
+);
+
 module.exports = {
 	customBuild: {
 		esbuild: {
 			bundle: true,
-			entryNames: 'headless-discovery-web-min',
-			entryPoints: [path.resolve('src', 'index.js')],
+			entryNames: '[name]',
+			entryPoints: {
+				'editor.worker': path.resolve(
+					monacoEditorPath,
+					'esm/vs/editor/editor.worker.js'
+				),
+				'graphql.worker': path.resolve(
+					monacoGraphqlPath,
+					'esm/graphql.worker.js'
+				),
+				'headless-discovery-web-min': path.resolve('src', 'index.js'),
+				'json.worker': path.resolve(
+					monacoEditorPath,
+					'esm/vs/language/json/json.worker.js'
+				),
+			},
 			loader: {
 				'.js': 'jsx',
+				'.ttf': 'file',
 			},
 			outdir,
-			plugins: [polyfillNode({})],
+			plugins: [
+				{
+					name: 'resolve-graphiql-deps',
+					setup(build) {
+						build.onResolve(
+							{filter: /^monaco-graphql\/esm\//},
+							(args) => {
+								if (args.path.endsWith('.js')) {
+									return null;
+								}
+
+								return {
+									path: require.resolve(args.path + '.js'),
+								};
+							}
+						);
+						build.onResolve({filter: /^@graphiql\//}, (args) => ({
+							path: require.resolve(args.path, {
+								paths: [path.resolve('node_modules')],
+							}),
+						}));
+					},
+				},
+				polyfillNode({}),
+			],
 			sourcemap: true,
 			target: ['es2020'],
 		},
