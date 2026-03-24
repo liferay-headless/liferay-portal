040306c5aac068c764f24f2346b08f2b9f235f37
Jaime Leon <jaime.leon@liferay.com>
2026-03-24 16:24:33 +0100

    LPD-78359 Implement LAR validation

--- modules/apps/export-import/export-import-rest-impl/src/main/java/com/liferay/exportimport/rest/internal/resource/v1_0/ImportProcessResourceImpl.java
+++ modules/apps/export-import/export-import-rest-impl/src/main/java/com/liferay/exportimport/rest/internal/resource/v1_0/ImportProcessResourceImpl.java
@@ -6,8 +6,15 @@
 package com.liferay.exportimport.rest.internal.resource.v1_0;
 
 import com.liferay.exportimport.kernel.background.task.BackgroundTaskExecutorNames;
+import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationSettingsMapFactoryUtil;
+import com.liferay.exportimport.kernel.configuration.constants.ExportImportConfigurationConstants;
+import com.liferay.exportimport.kernel.model.ExportImportConfiguration;
+import com.liferay.exportimport.kernel.service.ExportImportConfigurationLocalService;
+import com.liferay.exportimport.kernel.service.ExportImportLocalService;
+import com.liferay.exportimport.kernel.staging.Staging;
 import com.liferay.exportimport.rest.dto.v1_0.ImportProcess;
 import com.liferay.exportimport.rest.dto.v1_0.Status;
+import com.liferay.exportimport.rest.dto.v1_0.ValidationResult;
 import com.liferay.exportimport.rest.internal.util.PermissionUtil;
 import com.liferay.exportimport.rest.resource.v1_0.ImportProcessResource;
 import com.liferay.headless.delivery.dto.v1_0.util.CreatorUtil;
@@ -19,18 +26,30 @@ import com.liferay.portal.kernel.dao.orm.OrderFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.NoSuchBackgroundTaskException;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.service.LayoutService;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.vulcan.multipart.BinaryFile;
+import com.liferay.portal.vulcan.multipart.MultipartBody;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
 import com.liferay.staging.StagingGroupHelper;
 
+import java.io.Serializable;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -117,6 +136,57 @@ public class ImportProcessResourceImpl extends BaseImportProcessResourceImpl {
 				_getDynamicQuery(creatorId, siteId, search, status)));
 	}
 
+	@Override
+	public ValidationResult postSiteValidateImportFile(
+			Long siteId, MultipartBody multipartBody)
+		throws Exception {
+
+		PermissionUtil.checkPermission(contextCompany.getCompanyId(), siteId);
+
+		FileEntry fileEntry = _saveTempFile(siteId, multipartBody);
+
+		return _validateLarFile(siteId, fileEntry);
+	}
+
+	@Override
+	public ValidationResult postValidateImportFile(MultipartBody multipartBody)
+		throws Exception {
+
+		long groupId = _getCompanyGroupId();
+
+		PermissionUtil.checkPermission(contextCompany.getCompanyId(), groupId);
+
+		FileEntry fileEntry = _saveTempFile(groupId, multipartBody);
+
+		return _validateLarFile(groupId, fileEntry);
+	}
+
+	private ValidationResult _buildErrorResult(String messageKey) {
+		ValidationResult validationResult = new ValidationResult();
+
+		validationResult.setSuccess(() -> false);
+
+		String translatedMessage = _language.get(
+			contextAcceptLanguage.getPreferredLocale(), messageKey);
+
+		validationResult.setErrorMessages(
+			() -> new String[] {translatedMessage});
+
+		return validationResult;
+	}
+
+	private void _deleteTempFileEntries(long groupId, String folderName)
+		throws Exception {
+
+		String[] tempFileNames = _layoutService.getTempFileNames(
+			groupId, folderName);
+
+		for (String tempFileEntryName : tempFileNames) {
+			_layoutService.deleteTempFileEntry(
+				groupId, folderName, tempFileEntryName);
+		}
+	}
+
 	private List<BackgroundTask> _getBackgroundTasks(
 			Long creatorId, long groupId, Pagination pagination, String search,
 			Sort[] sorts, Integer status)
@@ -174,6 +244,19 @@ public class ImportProcessResourceImpl extends BaseImportProcessResourceImpl {
 		}
 
 		return dynamicQuery;
+	}
+
+	private FileEntry _saveTempFile(long groupId, MultipartBody multipartBody)
+		throws Exception {
+
+		BinaryFile binaryFile = multipartBody.getBinaryFile("file");
+
+		_deleteTempFileEntries(groupId, ImportProcessResource.class.getName());
+
+		return _layoutService.addTempFileEntry(
+			groupId, ImportProcessResource.class.getName(),
+			binaryFile.getFileName(), binaryFile.getInputStream(),
+			binaryFile.getContentType());
 	}
 
 	private void _setSorts(DynamicQuery dynamicQuery, Sort[] sorts) {
@@ -244,14 +327,72 @@ public class ImportProcessResourceImpl extends BaseImportProcessResourceImpl {
 		};
 	}
 
+	private ValidationResult _validateLarFile(long groupId, FileEntry fileEntry)
+		throws Exception {
+
+		Map<String, Serializable> parameterMap =
+			ExportImportConfigurationSettingsMapFactoryUtil.
+				buildImportLayoutSettingsMap(
+					contextUser.getUserId(), groupId, false, null,
+					new HashMap<String, String[]>(),
+					contextAcceptLanguage.getPreferredLocale(),
+					TimeZone.getDefault());
+
+		ExportImportConfiguration exportImportConfiguration =
+			_exportImportConfigurationLocalService.addExportImportConfiguration(
+				contextUser.getUserId(), groupId, null, null,
+				ExportImportConfigurationConstants.TYPE_IMPORT_LAYOUT,
+				parameterMap, new ServiceContext());
+
+		try {
+			_exportImportLocalService.validateImportLayoutsFile(
+				exportImportConfiguration, fileEntry.getContentStream());
+
+			return new ValidationResult() {
+				{
+					setFileEntryId(fileEntry::getFileEntryId);
+					setSuccess(() -> true);
+				}
+			};
+		}
+		catch (PortalException portalException) {
+			_layoutService.deleteTempFileEntry(
+				groupId, ImportProcessResource.class.getName(),
+				fileEntry.getFileName());
+
+			JSONObject jsonObject = _staging.getExceptionMessagesJSONObject(
+				LocaleUtil.getDefault(), portalException,
+				exportImportConfiguration);
+
+			return _buildErrorResult(jsonObject.getString("message"));
+		}
+		finally {
+			_exportImportConfigurationLocalService.
+				deleteExportImportConfiguration(exportImportConfiguration);
+		}
+	}
+
 	@Reference
 	private BackgroundTaskLocalService _backgroundTaskLocalService;
+
+	@Reference
+	private ExportImportConfigurationLocalService
+		_exportImportConfigurationLocalService;
+
+	@Reference
+	private ExportImportLocalService _exportImportLocalService;
 
 	@Reference
 	private Language _language;
 
 	@Reference
+	private LayoutService _layoutService;
+
+	@Reference
 	private Portal _portal;
+
+	@Reference
+	private Staging _staging;
 
 	@Reference
 	private StagingGroupHelper _stagingGroupHelper;
