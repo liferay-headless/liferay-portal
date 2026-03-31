974f72c8f71b773f2761d363946f0e15833ebfb2
Gabor Komaromi <gabor.komaromi@liferay.com>
2026-03-31 16:29:37 +0200

    LPD-82992 - align instance methods

--- modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/multipart/MultipartBody.java
+++ modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/multipart/MultipartBody.java
@@ -48,14 +48,14 @@ public class MultipartBody {
 	public <T> T getValueAsInstance(String key, Class<T> clazz)
 		throws IOException {
 
-		String valueAsString = getValueAsString(key);
+		T value = getValueAsNullableInstance(key, clazz);
 
-		if (valueAsString == null) {
+		if (value == null) {
 			throw new BadRequestException(
 				"Missing JSON property with the key: " + key);
 		}
 
-		return _parseValue(valueAsString, clazz);
+		return value;
 	}
 
 	public <T> T getValueAsNullableInstance(String key, Class<T> clazz)
