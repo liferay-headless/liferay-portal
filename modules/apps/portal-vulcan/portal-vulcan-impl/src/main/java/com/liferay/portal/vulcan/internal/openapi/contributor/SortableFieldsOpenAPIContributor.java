bf14ed5323f5fc3495e3fe28756d40894b896e57
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2026-07-12 22:02:54 +0200

    LPD-65574 Expose sortable fields in the OpenAPI spec via x-sortable
    Consumers building on top of the REST APIs (e.g. constructing a DataSet) had no way to know which fields actually support sorting, so they could mark an unsortable field as sortable and only discover the mismatch at runtime. SortableFieldsOpenAPIContributor mirrors the existing FilterableFieldsOpenAPIContributor mechanism, adding an x-sortable vendor extension to each schema listing every field safe to sort by. Collection-typed fields are excluded, since sorting on a genuinely multi-valued indexed field relies on the search engine's implicit min/max reduction rather than a well-defined ordering.

---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/openapi/contributor/SortableFieldsOpenAPIContributor.java
@@ -0,0 +1,372 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.vulcan.internal.openapi.contributor;
+
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.odata.entity.CollectionEntityField;
+import com.liferay.portal.odata.entity.ComplexEntityField;
+import com.liferay.portal.odata.entity.EntityField;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.vulcan.openapi.OpenAPIContext;
+import com.liferay.portal.vulcan.openapi.contributor.OpenAPIContributor;
+import com.liferay.portal.vulcan.resource.EntityModelResource;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.Paths;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+
+import jakarta.ws.rs.core.MultivaluedHashMap;
+
+import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Magdalena Jedraszak
+ */
+@Component(service = OpenAPIContributor.class)
+public class SortableFieldsOpenAPIContributor implements OpenAPIContributor {
+
+	@Override
+	public void contribute(OpenAPI openAPI, OpenAPIContext openAPIContext)
+		throws Exception {
+
+		Components components = openAPI.getComponents();
+
+		if ((components == null) || (openAPIContext == null)) {
+			return;
+		}
+
+		Map<String, Schema> schemas = components.getSchemas();
+
+		if (MapUtil.isEmpty(schemas)) {
+			return;
+		}
+
+		Map<String, Map<String, Object>> schemaNameSortableFields =
+			new HashMap<>();
+
+		for (Schema schema : schemas.values()) {
+			Map<String, Object> sortableFields = _getSortableFields(
+				openAPIContext, schema);
+
+			schema.addExtension("x-sortable", sortableFields);
+
+			schemaNameSortableFields.put(schema.getName(), sortableFields);
+		}
+
+		Paths paths = openAPI.getPaths();
+
+		if (MapUtil.isEmpty(paths)) {
+			return;
+		}
+
+		for (PathItem pathItem : paths.values()) {
+			_setXSortable(schemaNameSortableFields, pathItem.getDelete());
+			_setXSortable(schemaNameSortableFields, pathItem.getGet());
+			_setXSortable(schemaNameSortableFields, pathItem.getHead());
+			_setXSortable(schemaNameSortableFields, pathItem.getOptions());
+			_setXSortable(schemaNameSortableFields, pathItem.getPatch());
+			_setXSortable(schemaNameSortableFields, pathItem.getPost());
+			_setXSortable(schemaNameSortableFields, pathItem.getPut());
+			_setXSortable(schemaNameSortableFields, pathItem.getTrace());
+		}
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, null, "(osgi.jaxrs.resource=true)",
+			(serviceReference, emitter) -> {
+				try {
+					if (!(_bundleContext.getService(serviceReference) instanceof
+							EntityModelResource)) {
+
+						return;
+					}
+
+					String apiVersion = (String)serviceReference.getProperty(
+						"api.version");
+					String entityClassName =
+						(String)serviceReference.getProperty(
+							"entity.class.name");
+					Object companyIdObject = serviceReference.getProperty(
+						"companyId");
+
+					if (companyIdObject instanceof List) {
+						for (Object object : (List<?>)companyIdObject) {
+							emitter.emit(
+								_encodeKey(
+									entityClassName, GetterUtil.getLong(object),
+									apiVersion));
+						}
+
+						return;
+					}
+
+					emitter.emit(
+						_encodeKey(
+							entityClassName,
+							GetterUtil.getLong(companyIdObject), apiVersion));
+				}
+				finally {
+					bundleContext.ungetService(serviceReference);
+				}
+			});
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private String _encodeKey(
+		String className, Long companyId, String version) {
+
+		String key = StringBundler.concat(
+			className, StringPool.POUND, GetterUtil.getString(version, "v1.0"));
+
+		if (Validator.isNull(companyId)) {
+			return key;
+		}
+
+		return key + StringPool.POUND + companyId;
+	}
+
+	private String _getClassName(String className, String schemaName) {
+		if (schemaName != null) {
+			return className + "#" + StringUtil.toLowerCase(schemaName);
+		}
+
+		return className;
+	}
+
+	private Map<String, EntityField> _getEntityFieldsMap(
+			OpenAPIContext openAPIContext, Schema schema)
+		throws Exception {
+
+		Map<String, Schema> properties = schema.getProperties();
+
+		if (properties == null) {
+			return null;
+		}
+
+		Schema xClassNameSchema = properties.get("x-class-name");
+
+		if (xClassNameSchema == null) {
+			return null;
+		}
+
+		String xClassNameDefault = (String)xClassNameSchema.getDefault();
+
+		if (Validator.isBlank(xClassNameDefault)) {
+			return null;
+		}
+
+		Schema xSchemaNameSchema = properties.get("x-schema-name");
+
+		String xSchemaName = null;
+
+		if (xSchemaNameSchema != null) {
+			xSchemaName = (String)xSchemaNameSchema.getDefault();
+		}
+
+		EntityModelResource entityModelResource = _getEntityModelResource(
+			CompanyThreadLocal.getCompanyId(),
+			_getClassName(xClassNameDefault, xSchemaName),
+			openAPIContext.getVersion());
+
+		if (entityModelResource == null) {
+			return null;
+		}
+
+		entityModelResource.setContextCompany(
+			_companyLocalService.getCompany(CompanyThreadLocal.getCompanyId()));
+
+		EntityModel entityModel = entityModelResource.getEntityModel(
+			new MultivaluedHashMap());
+
+		if (entityModel == null) {
+			return null;
+		}
+
+		return entityModel.getEntityFieldsMap();
+	}
+
+	private EntityModelResource _getEntityModelResource(
+		long companyId, String className, String version) {
+
+		String companyIdKey = _encodeKey(className, companyId, version);
+
+		if (_serviceTrackerMap.containsKey(companyIdKey)) {
+			return _serviceTrackerMap.getService(companyIdKey);
+		}
+
+		String key = _encodeKey(className, null, version);
+
+		if (_serviceTrackerMap.containsKey(key)) {
+			return _serviceTrackerMap.getService(key);
+		}
+
+		return null;
+	}
+
+	private Map<String, Object> _getSortableFields(
+			OpenAPIContext openAPIContext, Schema schema)
+		throws Exception {
+
+		Map<String, EntityField> entityFieldsMap = _getEntityFieldsMap(
+			openAPIContext, schema);
+
+		if (MapUtil.isEmpty(entityFieldsMap)) {
+			return Collections.emptyMap();
+		}
+
+		Map<String, Object> sortableFields = new TreeMap<>();
+
+		Set<EntityField> visitedEntityFields = new HashSet<>();
+
+		Queue<Map.Entry<String, EntityField>> queue = new LinkedList<>(
+			entityFieldsMap.entrySet());
+
+		while (!queue.isEmpty()) {
+			Map.Entry<String, EntityField> entry1 = queue.poll();
+
+			String fieldName = entry1.getKey();
+
+			if (StringUtil.count(fieldName, '/') >= 5) {
+				continue;
+			}
+
+			EntityField entityField = entry1.getValue();
+
+			if (entityField instanceof CollectionEntityField) {
+
+				// A collection field's sortable name resolves to the same
+				// indexed field used for filtering, but sorting on a
+				// multi-valued field relies on the search engine's implicit
+				// min/max reduction rather than a well-defined ordering, so
+				// it is left out of x-sortable.
+
+				continue;
+			}
+			else if (!(entityField instanceof ComplexEntityField)) {
+				sortableFields.put(
+					fieldName,
+					HashMapBuilder.put(
+						"type",
+						StringUtil.toLowerCase(
+							String.valueOf(entityField.getType()))
+					).build());
+
+				continue;
+			}
+
+			ComplexEntityField complexEntityField =
+				(ComplexEntityField)entityField;
+
+			if (!visitedEntityFields.add(complexEntityField)) {
+				continue;
+			}
+
+			Map<String, EntityField> complexEntityFieldEntityFieldsMap =
+				complexEntityField.getEntityFieldsMap();
+
+			for (Map.Entry<String, EntityField> entry2 :
+					complexEntityFieldEntityFieldsMap.entrySet()) {
+
+				queue.add(
+					new AbstractMap.SimpleEntry<>(
+						entry1.getKey() + "/" + entry2.getKey(),
+						entry2.getValue()));
+			}
+		}
+
+		return sortableFields;
+	}
+
+	private void _setXSortable(
+		Map<String, Map<String, Object>> schemaNameSortableFields,
+		Operation operation) {
+
+		if (operation == null) {
+			return;
+		}
+
+		List<String> tags = operation.getTags();
+
+		if (ListUtil.isEmpty(tags)) {
+			return;
+		}
+
+		List<Parameter> parameters = operation.getParameters();
+
+		if (ListUtil.isEmpty(parameters)) {
+			return;
+		}
+
+		Parameter sortParameter = null;
+
+		for (Parameter parameter : parameters) {
+			if (StringUtil.equals(parameter.getName(), "sort")) {
+				sortParameter = parameter;
+
+				break;
+			}
+		}
+
+		if (sortParameter == null) {
+			return;
+		}
+
+		Schema schema = sortParameter.getSchema();
+
+		if (schema == null) {
+			return;
+		}
+
+		schema.addExtension(
+			"x-sortable", schemaNameSortableFields.get(tags.get(0)));
+	}
+
+	private BundleContext _bundleContext;
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	private ServiceTrackerMap<String, EntityModelResource> _serviceTrackerMap;
+
+}
