da31f8e75c3631e2453c1de3b9e58878586110af
Petteri Karttunen <petteri.karttunen@liferay.com>
2026-09-18 13:06:23 +0300

    LPD-101772 mcp-server-rest-impl: Apply the new signature. Return null instead of blank for the SDK to omit instructions when empty

--- modules/apps/mcp/mcp-server-rest-impl/src/main/java/com/liferay/mcp/server/rest/internal/servlet/MCPServerServlet.java
+++ modules/apps/mcp/mcp-server-rest-impl/src/main/java/com/liferay/mcp/server/rest/internal/servlet/MCPServerServlet.java
@@ -214,7 +214,7 @@ public class MCPServerServlet extends HttpServlet {
 		).immediateExecution(
 			true
 		).instructions(
-			(String)values.get("instructions")
+			MapUtil.getString(values, "instructions", null)
 		).prompts(
 			_getSyncPromptSpecifications(companyId)
 		).tools(
@@ -519,7 +519,8 @@ public class MCPServerServlet extends HttpServlet {
 
 		try {
 			Tool tool = ToolSetUtil.getTool(
-				httpServletRequest, restrictFieldsMap, toolName, toolSetName);
+				httpServletRequest, false, restrictFieldsMap, toolName,
+				toolSetName);
 
 			return McpSchema.Tool.builder(
 			).description(
